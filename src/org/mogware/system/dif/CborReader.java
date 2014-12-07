88846145d1e93049dd5cdd38984da5e1f5f49e3b
Mogens Jensen <mjensen@sirsi.dk>
2014-12-07 13:34:29 +0100

    Fixed typo in function readUInt64

--- src/org/mogware/system/dif/CborReader.java
+++ src/org/mogware/system/dif/CborReader.java
@@ -247,7 +247,7 @@ public class CborReader implements Reader {
             byte[] buf = readFully(in, new byte[8]);
             return (buf[0] & 0xFFL) << 56 | (buf[1] & 0xFFL) << 48 |
                     (buf[2] & 0xFFL) << 40 | (buf[3] & 0xFFL) << 32 |
-                    (buf[4] & 0xFFL) << 24 | (buf[5] & 0xFFL) << 26 |
+                    (buf[4] & 0xFFL) << 24 | (buf[5] & 0xFFL) << 16 |
                     (buf[6] & 0xFFL) << 8 | (buf[7] & 0xFFL);
         }
 
