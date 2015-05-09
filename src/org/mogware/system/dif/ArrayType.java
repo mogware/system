c3ab393fdeac1e0793e8c300d4b2ad6b16a5b958
Mogens Jensen <mjensen@sirsi.dk>
2015-05-09 17:25:46 +0200

    Remove "abstract" keyword

--- src/org/mogware/system/dif/ArrayType.java
+++ src/org/mogware/system/dif/ArrayType.java
@@ -2,7 +2,7 @@ package org.mogware.system.dif;
 
 public interface ArrayType extends Target {
     public int size();
-    public abstract boolean add(Object value);
+    public boolean add(Object value);
     public Object get(int index);
     public void clear();
 }
